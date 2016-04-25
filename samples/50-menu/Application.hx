47f0f3f9659229a226fd6cbbb104c4994a2bfdcf
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-04-25 13:07:25 +0200

    Started a menu sample

---
+++ samples/50-menu/Application.hx
@@ -0,0 +1,35 @@
+import gengine.*;
+import gengine.components.*;
+import gengine.math.*;
+import gengine.graphics.*;
+
+class GameSystem extends System
+{
+    public function new()
+    {
+        super();
+    }
+
+    override public function update(dt:Float):Void
+    {
+        if(Gengine.getInput().getScancodePress(41))
+        {
+            Gengine.exit();
+        }
+    }
+}
+
+class Application
+{
+    public static function init()
+    {
+        Gengine.setWindowSize(new IntVector2(1024, 768));
+        Gengine.setWindowTitle("50-menu");
+        Gengine.setGuiFilename("gui/gui.html");
+    }
+
+    public static function start(engine:Engine)
+    {
+        engine.addSystem(new GameSystem(), 2);
+    }
+}
