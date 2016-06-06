fa5d7358b43669f93804874e91f9f4eb11b0298a
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-06-06 13:26:37 +0200

    Updated physic sample

--- samples/02-physics/Application.hx
+++ samples/02-physics/Application.hx
@@ -2,18 +2,67 @@ import gengine.*;
 import gengine.math.*;
 import gengine.components.*;
 
-class ExitSystem extends System
+class FactorySystem extends System
 {
+    private var engine:Engine;
+
     public function new()
     {
         super();
     }
 
+    override public function addToEngine(_engine:Engine)
+    {
+        engine = _engine;
+    }
+
+    public function spawnCrate(size:Float, position:Vector3)
+    {
+        var hsize = size * 0.5;
+        var e = new Entity();
+        e.add(new StaticSprite2D());
+        e.get(StaticSprite2D).setSprite(Gengine.getResourceCache().getSprite2D("crate.png", true));
+        e.get(StaticSprite2D).setDrawRect(new Rect(new Vector2(-hsize, -hsize), new Vector2(hsize, hsize)));
+        e.add(new RigidBody2D());
+        e.get(RigidBody2D).setBodyType(2);
+        e.add(new CollisionBox2D());
+        e.get(CollisionBox2D).setSize(new Vector2(size, size));
+        e.position = position;
+        engine.addEntity(e);
+    }
+}
+
+class InputSystem extends System
+{
+    private var engine:Engine;
+    private var camera:Camera;
+
+    public function new(_camera:Camera)
+    {
+        super();
+        camera = _camera;
+    }
+
+    override public function addToEngine(_engine:Engine)
+    {
+        engine = _engine;
+    }
+
     override public function update(dt:Float):Void
     {
-        if(Gengine.getInput().getScancodePress(41))
+        var input = Gengine.getInput();
+
+        if(input.getScancodePress(41))
         {
             Gengine.exit();
+        }
+
+        var mousePosition = input.getMousePosition();
+
+        if(input.getMouseButtonPress(1))
+        {
+            var mouseWorldPosition = camera.screenToWorldPoint(new Vector3(mousePosition.x / 800, mousePosition.y / 600, 0));
+            engine.getSystem(FactorySystem).spawnCrate(64, mouseWorldPosition);
         }
     }
 }
@@ -28,32 +77,32 @@ class Application
 
     public static function start(engine:Engine)
     {
-        engine.addSystem(new ExitSystem(), 0);
+        var cameraEntity = new Entity();
+        cameraEntity.add(new Camera());
+        cameraEntity.get(Camera).setOrthoSize(new Vector2(800, 600));
+        cameraEntity.get(Camera).setOrthographic(true);
+        engine.addEntity(cameraEntity);
+
+        engine.addSystem(new InputSystem(cameraEntity.get(Camera)), 0);
+        engine.addSystem(new FactorySystem(), 0);
 
         var sceneEntity = Gengine.getScene().getAsEntity();
         sceneEntity.add(new PhysicsWorld2D());
         sceneEntity.get(PhysicsWorld2D).setGravity(new Vector2(0, -10000000000));
 
-        var e = new Entity();
-        e.add(new StaticSprite2D());
-        var staticSprite2D:StaticSprite2D = e.get(StaticSprite2D);
-        staticSprite2D.setSprite(Gengine.getResourceCache().getSprite2D("logo.png", true));
-        e.add(new RigidBody2D());
-        e.get(RigidBody2D).setBodyType(2);
-        e.get(RigidBody2D).setMass(200);
-        e.add(new CollisionCircle2D());
-        e.get(CollisionCircle2D).setRadius(128);
-        e.get(CollisionCircle2D).setFriction(0);
-        e.position = new Vector3(0, 300, 0);
-        engine.addEntity(e);
 
         var e = new Entity();
         e.add(new RigidBody2D());
         e.add(new CollisionBox2D());
-        e.get(CollisionBox2D).setSize(new Vector2(600, 64));
-        e.position = new Vector3(0, -300, 0);
+        e.get(CollisionBox2D).setSize(new Vector2(512, 512));
+        e.add(new StaticSprite2D());
+        e.get(StaticSprite2D).setSprite(Gengine.getResourceCache().getSprite2D("crate.png", true));
+        e.get(StaticSprite2D).setDrawRect(new Rect(new Vector2(-256, -256), new Vector2(256, 256)));
+        e.position = new Vector3(0, -400, 0);
         engine.addEntity(e);
 
         Gengine.getRenderer().getDefaultZone().setFogColor(new Color(0.5, 0.5, 0.5, 1));
+
+        engine.getSystem(FactorySystem).spawnCrate(64, new Vector3(0, 300, 0));
     }
 }
