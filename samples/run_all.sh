ece7a831ba072da3bb06526fa5a4cc8f3dfbd07e
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-06-18 07:48:52 +0200

    Clean

--- samples/run_all.sh
+++ samples/run_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "[gengine:tests] Running all tests ..."
+echo "[gengine-samples] Running all samples ..."
 
 for dir in `ls`;
 do
     test -d "$dir" || continue
     cd $dir
-    echo "[gengine:tests] Running <$dir> ..."
+    echo "[gengine-samples] Running <$dir> ..."
     gengine-run $1
     cd ..
 done
