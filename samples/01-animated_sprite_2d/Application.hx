d39bec78d100f0a42e374dba8298fe66585f0f8f
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-12-03 10:56:02 +0100

    Added sample

---
+++ samples/01-animated_sprite_2d/Application.hx
@@ -0,0 +1,54 @@
+import gengine.*;
+import gengine.math.*;
+import gengine.components.*;
+
+class ExitSystem extends System
+{
+    public function new()
+    {
+        super();
+    }
+
+    override public function update(dt:Float):Void
+    {
+        if(Gengine.getInput().getScancodePress(41))
+        {
+            Gengine.exit();
+        }
+    }
+}
+
+class Application
+{
+    public static function init()
+    {
+        Gengine.setWindowSize(new IntVector2(800, 600));
+        Gengine.setWindowTitle("01-animated_sprite_2d");
+    }
+
+    public static function start(engine:Engine)
+    {
+        engine.addSystem(new ExitSystem(), 0);
+
+        var e = new Entity();
+        e.add(new AnimatedSprite2D());
+        var animatedSprite2D:AnimatedSprite2D = e.get(AnimatedSprite2D);
+        var animationSet = Gengine.getResourceCache().getAnimationSet2D("spriter/player.scml", true);
+        animatedSprite2D.setAnimationSet(animationSet);
+        animatedSprite2D.setAnimation("walk");
+        e.position = new Vector3(-200, -200, 0);
+        engine.addEntity(e);
+
+        var e = new Entity();
+        e.add(new AnimatedSprite2D());
+        var animatedSprite2D:AnimatedSprite2D = e.get(AnimatedSprite2D);
+        var animationSet = Gengine.getResourceCache().getAnimationSet2D("spine/spineboy.json", true);
+        animatedSprite2D.setAnimationSet(animationSet);
+        animatedSprite2D.setAnimation("walk");
+        e.position = new Vector3(200, -200, 0);
+        e.scale = new Vector3(0.5, 0.5, 1);
+        engine.addEntity(e);
+
+        Gengine.getRenderer().getDefaultZone().setFogColor(new Color(1,1,1,1));
+    }
+}
