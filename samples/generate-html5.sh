f658f326c8d8377311497aed4311a0c7fb22eaac
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-23 13:48:47 +0100

    Added generated index.html

--- samples/generate-html5.sh
+++ samples/generate-html5.sh
@@ -2,6 +2,8 @@
 
 echo "[gengine-samples] Running all samples ..."
 
+HTML="<html><body>"
+
 for dir in `ls`;
 do
     test -d "$dir" || continue
@@ -9,4 +11,10 @@ do
     echo "[gengine-samples] Generating HTML5 for <$dir> ..."
     gengine-pack --html5 $1
     cd ..
+
+    HTML="${HTML}<a href='./samples/${dir}/packed-html5/'>${dir}</a><br/>"
 done
+
+HTML="${HTML}</body></html>"
+
+echo ${HTML} > ../index.html
